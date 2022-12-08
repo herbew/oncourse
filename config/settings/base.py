8dd227b89450fff23ca60b11bbd03b2adec7121d
herbew <herbew@gmail.com>
2022-12-08 08:46:40 +0700

    change to mandatory

--- config/settings/base.py
+++ config/settings/base.py
@@ -385,9 +385,9 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool('DJANGO_ACCOUNT_ALLOW_REGISTRATION', True)
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_AUTHENTICATION_METHOD = 'username'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_REQUIRED = False #True
+ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "optional" #'mandatory', 'optional'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory' #, 'optional'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = 'oncourse.apps.users.adapters.AccountAdapter'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
