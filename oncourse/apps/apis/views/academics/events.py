61e5d35fef7d42a7574063b2b1e9ed53b26caeb1
herbew <herbew@gmail.com>
2022-12-10 15:11:04 +0700

    debug request.user

--- oncourse/apps/apis/views/academics/events.py
+++ oncourse/apps/apis/views/academics/events.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from oncourse.apps.academics.models.events import Event, StudentEvent
+from oncourse.apps.apis.views.academics.students.permissions import(
+    StudentPermission) 
 
 from oncourse.apps.academics.serializers.events import StudentEventSerializer
 
@@ -18,8 +20,12 @@ class StudentEventList(generics.ListCreateAPIView):
     permission_classes = [AllowAny]
     
     def list(self, request):
+        x = request.user
+        
         queryset = StudentEvent.objects.filter(
             user_traceability__user=request.user)
+        
+        queryset = StudentEvent.objects.all()
         
         serializer = StudentEventSerializer(queryset,many=True)
         return Response(serializer.data)
