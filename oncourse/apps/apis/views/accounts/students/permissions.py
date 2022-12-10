4b3348a81e2e84488f16a739cbb49f253a3f1691
herbew <herbew@gmail.com>
2022-12-10 15:49:30 +0700

    update comment

--- oncourse/apps/apis/views/accounts/students/permissions.py
+++ oncourse/apps/apis/views/accounts/students/permissions.py
@@ -12,7 +12,7 @@ class StudentPermission(permissions.BasePermission):
     Student Permission
     """
     def has_permission(self, request, view):
-        # Check UserTraceability if registered
+        # Check UserTraceability if registered with self registered
         approved = UserTraceability.objects.filter(user=self.request.user).exists()
         
         return approved
