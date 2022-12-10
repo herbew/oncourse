a9409cc399366a789d3f108784b1c3519b4d51a8
herbew <herbew@gmail.com>
2022-12-10 16:34:54 +0700

    generics.ListAPIView

--- oncourse/apps/apis/views/academics/events.py
+++ oncourse/apps/apis/views/academics/events.py
@@ -14,21 +14,21 @@ from oncourse.apps.academics.serializers.events import StudentEventSerializer
 
 log = logging.getLogger(__name__)
 
-class StudentEventList(generics.ListCreateAPIView):
+class StudentEventList(generics.ListAPIView):
     """Get Event base student(UserTraceAbility(user))
     """
     serializer_class = StudentEventSerializer
     permission_classes = [AllowAny]
     
-    def list(self, request):
+    def get_queryset(self):
         
         username = self.request.query_params.get('username')
         
         queryset = StudentEvent.objects.filter(
             user_traceability__user__username=username)
         
-        serializer = StudentEventSerializer(queryset,many=True)
-        return Response(serializer.data)
+        
+        return queryset
 
 
 
