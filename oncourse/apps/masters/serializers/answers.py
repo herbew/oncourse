069b0334419c0643c3c65492625267f5cb8fa3e9
herbew <herbew@gmail.com>
2022-12-10 21:16:58 +0700

    skipp task

--- oncourse/apps/masters/serializers/answers.py
+++ oncourse/apps/masters/serializers/answers.py
@@ -7,7 +7,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from rest_framework import serializers
 
-from oncourse.apps.masters.serializers.tasks import TaskSerializer
+# from oncourse.apps.masters.serializers.tasks import TaskSerializer
 
 from oncourse.apps.masters.models.answers import Answer
 
@@ -17,7 +17,7 @@ class AnswerSerializer(serializers.ModelSerializer):
     option = serializers.ChoiceField(choices=Answer.TASK_OPTION_CHOICES)
     class Meta:
         model = Answer
-        fields = ['id','task','option', 'detailed']
+        fields = ['id','option', 'detailed']
         
         validators = [
             serializers.UniqueTogetherValidator(
